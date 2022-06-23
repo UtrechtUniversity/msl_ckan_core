f4c627356989ed5a3f40f045dc46ddbd781b8bc9
Samsh001 <l.samshuijzen@uu.nl>
2022-06-23 13:17:09 +0200

    test setup.py changes

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
     # have to be included in MANIFEST.in as well.
     include_package_data=True,
     package_data={
+        "msl_ckan": ["*.png"]
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
