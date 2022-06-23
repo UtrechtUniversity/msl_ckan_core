d4c15ad6f7ed43b762e680ad1b1fc2e2c04a1ea1
Samsh001 <l.samshuijzen@uu.nl>
2022-06-23 13:27:54 +0200

    test setup.py changes

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # have to be included in MANIFEST.in as well.
     include_package_data=True,
     package_data={
-        "ckanext": ["*.png"]
+        "msl_ckan.public": ["*.png"]
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
